c168c45ad07bf1c52d78c8867e6cc3c7a6890955
Violet-VE <lolico.violet@gmail.com>
2021-09-23 21:02:18 +0800

    初始化项目

---
+++ Source/PluginsDev/PluginsDevGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginsDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINSDEV_API APluginsDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
